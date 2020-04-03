eb0f41dcc130285fc8153795e6d95e2c99e3b931
CrizR <risleychris@gmail.com>
2020-04-03 16:25:26 -0400

    Adding functionality to read from CSV for names

--- run.py
+++ run.py
@@ -5,12 +5,13 @@ import shutil
 import csv
 import os
 import time
+import json
 
 
 class ArchiveUtility(object):
     OUTPUT_FILE = 'archive'
 
-    def __init__(self, directory, ap, sn, dp, file_del):
+    def __init__(self, directory, ap, sn, dp, file_del, csv_path, oc, dc):
         if directory[0] != file_del:
             self.directory = file_del + directory
         else:
@@ -20,6 +21,13 @@ class ArchiveUtility(object):
         self.dp = dp
         self.asset_number = 1000
         self.file_del = file_del
+        if csv_path:
+            self.with_csv = True
+        else:
+            self.with_csv = False
+        self.csv_path = csv_path
+        self.oc = int(oc)
+        self.dc = int(dc)
         self.created_dirs = {}
 
     def rename_file(self, file_name):
@@ -27,6 +35,53 @@ class ArchiveUtility(object):
                + "_" + file_name
 
     def run(self):
+        if self.with_csv:
+            self._run_with_csv()
+        else:
+            self._run_default()
+
+    def _run_with_csv(self):
+        now = str(round(time.time() * 1000))
+        csvfile = open(self.csv_path, "r")
+        csvfile.read(1)
+        name_map = {}
+        for line in csvfile.readlines():
+            line = line.split(",")
+            original_name = line[self.oc]
+            new_name = line[self.dc]
+            name_map[original_name] = new_name
+
+        with open('test.json', "w+") as file:
+            file.writelines(json.dumps(name_map))
+        try:
+            ArchiveUtility.rm_dir(self.OUTPUT_FILE)
+            ArchiveUtility.create_dir(self.OUTPUT_FILE)
+            path = os.getcwd() + self.directory + self.file_del + "**"
+            files_to_change = list(filter(lambda x: not os.path.isdir(x), glob(path, recursive=True)))
+            if not files_to_change:
+                ArchiveUtility.rm_dir(self.OUTPUT_FILE)
+                self.clean(csvfile, "No files to archive found in: " + self.directory)
+            for index, f_name in enumerate(files_to_change):
+                fsplit = f_name.split(self.file_del)
+                old_name = fsplit[-1]
+                if old_name in name_map:
+                    old_dirs_set = fsplit[self.get_depth_to_base(fsplit[1:]):-1]
+                    old_dirs = self.file_del.join(old_dirs_set)
+                    new_name = name_map[old_name]
+                    new_location = self.OUTPUT_FILE + self.file_del + old_dirs + self.file_del + new_name
+                    if old_dirs not in self.created_dirs:
+                        self.create_directories(old_dirs_set)
+                    copyfile(f_name, new_location)
+                    print("TEST")
+                    self.asset_number += 1
+                    ArchiveUtility.print_progress_bar(index + 1, len(files_to_change))
+            self.clean(csvfile, "Finished. Your archived files can be found in "
+                       + self.OUTPUT_FILE + "-" + now + "|| 00111100 00110011")
+        except Exception as e:
+            ArchiveUtility.rm_dir(self.OUTPUT_FILE)
+            self.clean(csvfile, "Error: " + str(e))
+
+    def _run_default(self):
         now = str(round(time.time() * 1000))
         csv_filename = self.OUTPUT_FILE + "-" + now + ".csv"
         csvfile = open(csv_filename, "w+")
@@ -35,7 +90,7 @@ class ArchiveUtility(object):
             ArchiveUtility.create_dir(self.OUTPUT_FILE)
             path = os.getcwd() + self.directory + self.file_del + "**"
             files_to_change = list(filter(lambda x: not os.path.isdir(x), glob(path,
-                                                                                    recursive=True)))
+                                                                               recursive=True)))
             csv_writer = csv.writer(csvfile, delimiter=',', quotechar='|',
                                     quoting=csv.QUOTE_MINIMAL)
             csv_writer.writerow(["Original Name", "Archive Name", "Original Directory", "Archive Directory"])
@@ -133,9 +188,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Archive Utility')
     parser.add_argument('-dir', '--directory',
                         help='File directory containing all the files (can include subdirectories)', required=True)
+
+    # Without CSV
     parser.add_argument('-ap', '--assetPrefix', help='Prefix to asset number', required=False)
-    parser.add_argument('-sn', '--sourceName', help='Name of Source', required=True)
+    parser.add_argument('-sn', '--sourceName', help='Name of Source', required=False)
     parser.add_argument('-dp', '--directoryPrefix', help='Prefix for archived directories', required=False)
+
+    # With CSV
+    parser.add_argument('-cp', '--csvPath', help='The file path of the csv to use', default=None, required=False)
+    parser.add_argument('-oc', '--originalColumn', help='The column number to use as the file name key',
+                        default=None,
+                        required=False)
+    parser.add_argument('-dc', '--destinationColumn', help='The column number to use as the file names new value',
+                        default=None,
+                        required=False)
+
     args = parser.parse_args()
 
-    ArchiveUtility(args.directory, args.assetPrefix, args.sourceName, args.directoryPrefix, file_del).run()
+    ArchiveUtility(args.directory, args.assetPrefix, args.sourceName, args.directoryPrefix, file_del,
+                   args.csvPath, args.originalColumn, args.destinationColumn).run()
